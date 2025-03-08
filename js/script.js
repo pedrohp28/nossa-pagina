3094d102d0b4721b0856508d246cbd821d17d60d
pedrohp28 <pedrohp36@gmail.com>
2025-03-08 11:51:03 -0300

    commit alteração

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@ buttons.forEach((btn) => {
         image.classList.add("changing");
         image.setAttribute("src", `img/iphone_${id}.jpg`);
 
-        console.log('hello')
 
         setTimeout(() =>{
             image.classList.toggle("changing");
